7274fb1d50b3539320b50900d783956262997f99
ChingRu <contact@chingru.me>
2026-01-20 14:31:03 +0800

    using vite 7

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   optimizeDeps: {
     exclude: ["@tanstack/start-server-core"],
